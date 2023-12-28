a7dff1d713931bb55b55d0cc33e70aa6ced4b79b
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2023-12-28 08:56:22 +0200

    Create useReducer and fetch data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
 
+const initialState = {
+  questions: [],
+  // loading,error,ready,active,finished
+  status: "loading",
+};
 
-function App() {
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailied":
+      return{
+        ...state,
+        status: 'error'
+      }
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecived", payload: data }))
+      .catch((err) => dispatch({type: 'dataFailied'}));
+  }, []);
   return (
-    <div className="App">
-      Hello world
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Qustion?</p>
+      </Main>
     </div>
   );
 }
-
-export default App;
